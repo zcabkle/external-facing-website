376a78af73239d28d7fd66fa4c4b99791f4749c2
zcabkle <73954803+zcabkle@users.noreply.github.com>
2023-01-29 17:41:25 +0000

    Foodbanks page done for now

--- client/src/components/FoodbankListItem.js
+++ client/src/components/FoodbankListItem.js
@@ -7,57 +7,41 @@ function FoodbankListItem({ name, address, image, guid }) {
   const source = 'data:image/png;base64,'.concat(' ').concat(image);
   const url = '/foodbanks/'.concat(guid);
   return (
-
     <div className='list-item'>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          p: 1,
-          m: 1,
-          borderRadius: 1,
-          minHeight: '100%',
-          maxHeight: '100%',
-          alignItems: 'center'
-        }}
-      >
 
-        <Grid container sx={{
-          spacing: 0.5,
-          alignItems: 'center'
-        }}>
-          <Grid item xs={3}>
-
-
-            <Box
-              component="img"
-              sx={{
-                height: 60,
-                width: 60,
-                borderRadius: '10%'
-              }}
-              alt="Foodbank."
-              src={source}
-            />
-
-          </Grid>
-          <Grid item xs={8}>
-            <Box>
-              {name}
-              {address}
-            </Box>
-          </Grid>
-          <Grid item xs={1}>
-            <Box>
-              <a href={url}><ArrowCircleRightOutlinedIcon style={{ color: '#00126b' }} /></a>
-              
-            </Box>
-          </Grid>
+      <Grid container sx={{
+        spacing: 0.5,
+        alignItems: 'center',
+      }}>
+        <Grid item xs={3} style={{textAlign: "center"}}>
+          <Box
+            component="img"
+            sx={{
+              height: '80%',
+              width: '80%',
+              maxHeight: '100px',
+              maxWidth: '100px',
+              borderRadius: '10%',
+            }}
+            alt="Foodbank."
+            src={source}
+          />
         </Grid>
 
-      </Box>
+        <Grid item xs={8}>
+          <Box>
+            <p>{name}</p>
+            <p>{address}</p>
+          </Box>
+        </Grid>
 
+        <Grid item xs={1} >
+          <Box>
+            <a href={url}><ArrowCircleRightOutlinedIcon style={{ color: '#00126b' }} /></a>
+          </Box>
+        </Grid>
 
+      </Grid>
     </div>
   );
 }
